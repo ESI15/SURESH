325803ef62dba6cf982129b2a859ae2df3c560d1
Suresh Reddy <cts@example.com>
2015-12-08 20:41:26 +0530

    Initial merge

---
+++ Auto_Merge.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Automatically merge the last commit through the following branches:
+#Hotfix-> Integration -> master
+
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+LAST_COMMIT=$(git rev-list -1 HEAD)
+
+echo Automatically merging commit $LAST_COMMIT from $CURRENT_BRANCH rippling to master
+
+case $CURRENT_BRANCH in
+Hotfix)
+
+git checkout master
+
+git merge $CURRENT_BRANCH
+
+git push
+
+git checkout Integration
+
+git merge $CURRENT_BRANCH
+
+git push
+
+git checkout $CURRENT_BRANCH
+
+;;
+esac
